6dc3aff633002fac7c20b5b53c0ebf3dd40dcdfd
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "communication_service_email_domain_associations" {
   description = "All communication_service_email_domain_association resources"
   value       = azurerm_communication_service_email_domain_association.communication_service_email_domain_associations
 }
+output "communication_service_email_domain_associations_communication_service_id" {
+  description = "List of communication_service_id values across all communication_service_email_domain_associations"
+  value       = [for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : v.communication_service_id]
+}
+output "communication_service_email_domain_associations_email_service_domain_id" {
+  description = "List of email_service_domain_id values across all communication_service_email_domain_associations"
+  value       = [for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : v.email_service_domain_id]
+}
 
