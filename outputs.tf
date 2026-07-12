de84a3efe3f1b4e92b4355022a86feea96ac4961
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "communication_service_email_domain_associations_id" {
+  description = "Map of id values across all communication_service_email_domain_associations, keyed the same as var.communication_service_email_domain_associations"
+  value       = { for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : k => v.id }
+}
 output "communication_service_email_domain_associations_communication_service_id" {
   description = "Map of communication_service_id values across all communication_service_email_domain_associations, keyed the same as var.communication_service_email_domain_associations"
   value       = { for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : k => v.communication_service_id }
