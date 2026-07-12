9a579498462253f819060b398132908455769e57
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "communication_service_email_domain_associations" {
-  description = "All communication_service_email_domain_association resources"
-  value       = azurerm_communication_service_email_domain_association.communication_service_email_domain_associations
-}
 output "communication_service_email_domain_associations_communication_service_id" {
-  description = "List of communication_service_id values across all communication_service_email_domain_associations"
-  value       = [for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : v.communication_service_id]
+  description = "Map of communication_service_id values across all communication_service_email_domain_associations, keyed the same as var.communication_service_email_domain_associations"
+  value       = { for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : k => v.communication_service_id }
 }
 output "communication_service_email_domain_associations_email_service_domain_id" {
-  description = "List of email_service_domain_id values across all communication_service_email_domain_associations"
-  value       = [for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : v.email_service_domain_id]
+  description = "Map of email_service_domain_id values across all communication_service_email_domain_associations, keyed the same as var.communication_service_email_domain_associations"
+  value       = { for k, v in azurerm_communication_service_email_domain_association.communication_service_email_domain_associations : k => v.email_service_domain_id }
 }
 
